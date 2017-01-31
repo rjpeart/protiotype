e32b0b045450fc28a10b326ff284f69c6f135a41
Oliver Meili <omi@ieee.org>
2017-01-31 14:26:24 +0000

    Added a csv option for download for offline analysis

--- kismet-bridge/scanner.js
+++ kismet-bridge/scanner.js
@@ -1,20 +1,22 @@
 var Kismet = require('./lib/kismet.js')
 var http = require('http');
+var url = require('url');
 
-var timeoutValue = 60;
+var timeoutValue = 60*1000;  // timeout set to 60s
+var taskPeriod = 2000;
 
 var k = new Kismet()
 
 var clients = {};
 
-timeout = function(){
+periodicTask = function(){
 	var now = Date.now()
 	var keys = Object.keys(clients);
 	for (var i = keys.length-1; i >= 0; i--){
 		var key = keys[i];
-		//console.log('now = ' + now/1000 + ' lasttime = ' + clients[key].lasttime + " difference = " +  (now/1000 - clients[key].lasttime));
-		if (now/1000 - clients[key].lasttime > timeoutValue){
-			console.log('client ' + clients[key].mac + ' disappeared');
+//		console.log('now = ' + now + ' lasttime = ' + clients[key].lasttime + " difference = " +  (now - clients[key].lasttime));
+		if (now - clients[key].lasttime > timeoutValue){
+			console.log('client ' + clients[key].mac + ' disappeared, stayed ' + ((clients[key].lasttime - clients[key].firsttime)/1000) + ' seconds');
 			delete clients[key];
 		}	
 	}
@@ -22,7 +24,7 @@ timeout = function(){
 
 // only a "connect" event, not ready until "ready"
 k.on('connect',function(){
-    console.log('connected!')
+    console.log('connected!');
 });
 
 k.on('ready',function(){
@@ -35,31 +37,46 @@ k.on('ready',function(){
 });
 
 k.on('CLIENT',function(fields){
-    if( fields.bssid != fields.mac ){
-//        console.log(
-//            'Kismet sees client: ' + fields.bssid
-//            + ' type: ' + k.types.lookup('client',fields.type)
-//            + ' mac: ' + fields.mac +' '+ JSON.stringify(fields)
-//        )
-	if (!( fields.mac in clients )){
-		console.log('client ' + fields.mac + ' found')
-	}
-	clients[fields.mac] = {
-          mac: fields.mac,
-          firsttime: fields.firsttime,
-          lasttime: fields.lasttime,
-	  signal: fields.signal_dbm,
-	  manufacturer: fields.manuf,
-        };
-   }
+	if( fields.bssid != fields.mac ){
+		var now = Date.now();
+//      	console.log(
+//            		'Kismet sees client: ' + fields.bssid
+//            		+ ' type: ' + k.types.lookup('client',fields.type)
+//            		+ ' mac: ' + fields.mac +' '+ JSON.stringify(fields)
+//        	);
+		if (!( fields.mac in clients )){
+			console.log('client ' + fields.mac + ' found');
+			clients[fields.mac] = {
+	        		mac: fields.mac,
+          			firsttime: now,
+	  			signal: fields.signal_dbm,
+	  			manufacturer: fields.manuf,
+        		};
+		}
+		clients[fields.mac].lasttime = now;
+    	}
 });
 
-var t = setInterval(timeout, 2000)
+var t = setInterval(periodicTask, taskPeriod);
 
 k.connect();
 
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(clients));
+	var params = url.parse(req.url, true);
+	if ("csv" in params.query){
+		res.writeHead(200, {'Content-Type': 'text/csv', 'Content-Disposition': 'inline; filename="clients.csv"'});
+		res.write('mac,firsttime,lasttime,signal,manufacturer\r\n');
+		for (var key in clients){
+			var client = clients[key];
+			res.write(client.mac + "," + client.firsttime + "," + client.lasttime + "," + client.signal + "," + client.manufacturer + "\r\n");
+		}
+		res.end();
+	}
+	else
+	{
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(clients));
+	}
 }).listen(9615);
 
