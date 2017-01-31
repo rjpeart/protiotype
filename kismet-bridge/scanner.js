70d4b3c6bf8d342443131c1cc8119a53ab4fd18b
Oliver Meili <omi@ieee.org>
2017-01-31 10:32:07 +0000

    Added events for new clients and clients which are no longer seen

--- kismet-bridge/scanner.js
+++ kismet-bridge/scanner.js
@@ -1,9 +1,23 @@
 var Kismet = require('./lib/kismet.js')
 var http = require('http');
 
+var timeoutValue = 5*1000;
+
 var k = new Kismet()
 
 var clients = {};
+
+timeout = function(){
+	var now = Date.now()
+	var keys = Object.keys(clients);
+	for (var i = keys.length-1; i >= 0; i--){
+		var key = keys[i];
+		if (now/1000 - clients[key].lasttime > timeoutValue){
+			console.log('client ' + JSON.stringify(clients[key]) + ' disappeared');
+			delete clients[key];
+		}	
+	}
+}
 
 // only a "connect" event, not ready until "ready"
 k.on('connect',function(){
@@ -21,19 +35,25 @@ k.on('ready',function(){
 
 k.on('CLIENT',function(fields){
     if( fields.bssid != fields.mac ){
-        console.log(
-            'Kismet sees client: ' + fields.bssid
-            + ' type: ' + k.types.lookup('client',fields.type)
-            + ' mac: ' + fields.mac +' '+ JSON.stringify(fields)
-        )
+//        console.log(
+//            'Kismet sees client: ' + fields.bssid
+//            + ' type: ' + k.types.lookup('client',fields.type)
+//            + ' mac: ' + fields.mac +' '+ JSON.stringify(fields)
+//        )
+	if (!( fields.mac in clients )){
+		console.log('client ' + fields.mac + ' found')
+	}
 	clients[fields.mac] = {
           mac: fields.mac,
           firsttime: fields.firsttime,
           lasttime: fields.lasttime,
 	  signal: fields.signal_dbm,
+	  manufacturer: fields.manuf,
         };
    }
 });
+
+var t = setInterval(timeout, 2000)
 
 k.connect();
 
