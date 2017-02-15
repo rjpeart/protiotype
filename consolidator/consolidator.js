b4cb993ccdbf5a7a283558526478f27ebb3434e0
Oliver Meili <omi@ieee.org>
2017-02-15 23:20:08 +0800

    Fixed starting http server when using MQTT

--- consolidator/consolidator.js
+++ consolidator/consolidator.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var url = require('url');
 var mqtt = require('mqtt');
 
+var use_mqtt = true;
+
 var sensor_env = {};
 var sensor_wifi = {};
 
@@ -37,56 +39,59 @@ client.on('connect', function() {
 		});
 
 client.on('error', function() {
-		http.createServer( function (req, res) {
-
-				var pathname = url.parse(req.url).pathname;
-				var body_json = {};
-
-				if (req.method == 'POST')
-				{
-				console.log('Received a POST request');
-				var body = '';
-				req.on('data', function(data) {
-						body += data;
-						});
-				req.on('end', function() {
-						body_json = JSON.parse(body);
-						if (pathname == '/environment')
-						{
-						sensor_env = body_json;
-						console.log('Environment values: ' + JSON.stringify(sensor_env));
-						}
-						else if (pathname == '/clients')
-						{
-						sensor_wifi = body_json;
-						console.log('Clients: ' + JSON.stringify(sensor_wifi));
-						}
-						else if (pathname == '/clients/event')
-						{
-						console.log('Event: ', JSON.stringify(body_json));
-						}
-						});
-				}
-
-				if (req.method == 'GET')
-				{
-					console.log('GET: Handling request for ' + pathname);
-
-					if (pathname == "/environment")
-					{
-						console.log("Received get for environment data: " + JSON.stringify(sensor_env));
-						res.writeHead(200, {'Content-Type': 'text/json'});
-						res.write(JSON.stringify(sensor_env));
-						res.end();
-					}
-					else if (pathname == '/clients')
-					{
-						console.log("Received get for client data: " + JSON.stringify(sensor_wifi));
-						res.writeHead(200, {'Content-Type': 'text/json'});
-						res.write(JSON.stringify(sensor_wifi));
-						res.end();
-					}
-				}		
-
-		}).listen(8000);
+	use_mqtt = false;
 });
+
+
+http.createServer( function (req, res) {
+
+		var pathname = url.parse(req.url).pathname;
+		var body_json = {};
+
+		if ((!use_mqtt) && (req.method == 'POST'))
+		{
+			console.log('Received a POST request');
+			var body = '';
+			req.on('data', function(data) {
+				body += data;
+			});
+			req.on('end', function() {
+				body_json = JSON.parse(body);
+				if (pathname == '/environment')
+				{
+					sensor_env = body_json;
+					console.log('Environment values: ' + JSON.stringify(sensor_env));
+				}
+				else if (pathname == '/clients')
+				{
+					sensor_wifi = body_json;
+					console.log('Clients: ' + JSON.stringify(sensor_wifi));
+				}
+				else if (pathname == '/clients/event')
+				{
+					console.log('Event: ', JSON.stringify(body_json));
+				}
+			});
+		}
+
+		if (req.method == 'GET')
+		{
+			console.log('GET: Handling request for ' + pathname);
+
+			if (pathname == "/environment")
+			{
+				console.log("Received get for environment data: " + JSON.stringify(sensor_env));
+				res.writeHead(200, {'Content-Type': 'text/json'});
+				res.write(JSON.stringify(sensor_env));
+				res.end();
+			}
+			else if (pathname == '/clients')
+			{
+				console.log("Received get for client data: " + JSON.stringify(sensor_wifi));
+				res.writeHead(200, {'Content-Type': 'text/json'});
+				res.write(JSON.stringify(sensor_wifi));
+				res.end();
+			}
+		}		
+
+}).listen(8000);
